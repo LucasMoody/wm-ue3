d618f4e02cb9c405314bf8881ab9bd1346095e6c
alexpilipas <alex.pilipas@googlemail.com>
2015-05-28 14:39:17 +0000

    update

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -2,16 +2,27 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 
-var url = "http://www.chefkoch.de/rs/s0t29,28/Europa-Italien-Rezepte.html";
+var url = "http://www.chefkoch.de/rs/s0t28/Italien-Rezepte.html";
+
 
 request(url, function(err, resp, body){
   $ = cheerio.load(body);
-  links = $('a'); //jquery get all hyperlinks
+//  links = $('a'); //jquery get all hyperlinks
   
-  $(links).each(function(i, link){
-    console.log($(link).text() + ':\n  ' + $(link).attr('href'));
-  });
+ // $(links).each(function(i, link){
+  //  console.log($(link).text() + $(link).attr('href'));
+ // });
+  
+   $('.recipe-result-col-1').filter(function(){
+
+                var data = $(this);
+               
+                
+                console.log(data);
+
+            })
+  
 });
         
-    
+   
 

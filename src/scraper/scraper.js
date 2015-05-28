6a4822235342f7569dddd62166a73521c92394eb
alexpilipas <alex.pilipas@googlemail.com>
2015-05-28 17:55:45 +0000

    It's now possible to scrap the links on Italian receipt page

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -1,26 +1,18 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-
+// Scrap italian receipts
 var url = "http://www.chefkoch.de/rs/s0t28/Italien-Rezepte.html";
-
 
 request(url, function(err, resp, body){
   $ = cheerio.load(body);
-//  links = $('a'); //jquery get all hyperlinks
-  
- // $(links).each(function(i, link){
-  //  console.log($(link).text() + $(link).attr('href'));
- // });
-  
-   $('.recipe-result-col-1').filter(function(){
 
-                var data = $(this);
-               
-                
-                console.log(data);
-
-            })
+        
+     $('span.rating-small').each(function(i, element){
+      var a = $(this).prev();
+      var a = a.attr('href');
+      console.log("http://chefkoch.de/"+ a);
+    });
   
 });
         

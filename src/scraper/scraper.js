a79e92b8972c4d5b0404b6ae11c2808e61f321b1
alexpilipas <alex.pilipas@googlemail.com>
2015-05-28 18:37:10 +0000

    updated scraper scrapes 450 italian receipts

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -2,19 +2,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 // Scrap italian receipts
-var url = "http://www.chefkoch.de/rs/s0t28/Italien-Rezepte.html";
-
-request(url, function(err, resp, body){
-  $ = cheerio.load(body);
-
-        
-     $('span.rating-small').each(function(i, element){
-      var a = $(this).prev();
-      var a = a.attr('href');
-      console.log("http://chefkoch.de/"+ a);
-    });
-  
-});
-        
+var startpage = 0;
+var scrape = function(){
+    
+// loop iterates over 15 pages and each page has 30 receipts
+for(var i =0; i<15;i++){
    
-
+    var url = "http://www.chefkoch.de/rs/s" + startpage +"t28/Italien-Rezepte.html";
+    
+    request(url, function(err, resp, body){
+     $ = cheerio.load(body);
+     
+     $('span.rating-small').each(function(i,element){
+      var a = $(this).prev();
+      var url = "http://chefkoch.de"+a.attr('href');
+      
+      // How to trade an undefined link?
+      if(url==="http://chefkoch.deundefined"){
+          console.log("The link was undefined");
+      }else{
+           console.log(url);
+           // insert into mongoDB here
+           //  exports.storeWebpageSync("http://chefkoch.de"+ a,);
+      }
+     
+     });
+  
+    });
+     
+     startpage+=30;
+    }   
+}
+scrape();

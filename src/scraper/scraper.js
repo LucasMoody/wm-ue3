909cdd1468a7e666ae2a462d7e52c8b62a2dc06c
alexpilipas <alex.pilipas@googlemail.com>
2015-05-29 13:01:04 +0000

    updated scraper, not optimal yet

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -2,27 +2,36 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 // Scrape italian receipts
-var scrape = function(){
+var scrape = function(url,amount){
     var startpage = 0;
-// loop iterates over 15 pages and each page has 30 receipts
-for(var i =0; i<15;i++){
-   
-    var url = "http://www.chefkoch.de/rs/s" + startpage +"t28/Italien-Rezepte.html";
+    var oldPage=0;
+    var count =0;
+// loop iterates ..cant stop it
+for(var i =0; i<100;i++){
     
+
+      url = url.replace("s"+oldPage,"s"+startpage);
+       oldPage = startpage;
+     
+   
     request(url, function(err, resp, body){
      $ = cheerio.load(body);
      
      $('span.rating-small').each(function(i,element){
       var a = $(this).prev();
-      var url = "http://chefkoch.de"+a.attr('href');
+      var urlIntern = "http://chefkoch.de"+a.attr('href');
       
       // How to trade an undefined link?
-      if(url==="http://chefkoch.deundefined"){
+      if(urlIntern==="http://chefkoch.deundefined"){
           console.log("The link was undefined");
       }else{
-           console.log(url);
+          if(count<amount){
+               console.log(urlIntern);
            // insert into mongoDB here
            //  exports.storeWebpageSync("http://chefkoch.de"+ a,);
+           count++;
+          }
+          
       }
      
      });
@@ -45,7 +54,12 @@ var getContent = function(url){
     });
 }
 
-//scrape();
+
+
+
+// Call functions
+scrape("http://www.chefkoch.de/rs/s0t29,28/Europa-Italien-Rezepte.html",500);
+//scrape("http://www.chefkoch.de/rs/s0t101/Afrika-Rezepte.html",500);
 //getContent("http://www.chefkoch.de/rezepte/889601194503872/Knusprig-duenne-Pizza-mit-Chorizo-und-Mozzarella.html");
 
 

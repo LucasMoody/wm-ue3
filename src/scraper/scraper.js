02acac2f5d3072fe19dbbd433260b2a60386826e
alexpilipas <alex.pilipas@googlemail.com>
2015-06-04 14:23:27 +0000

    minor

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -25,13 +25,13 @@ function scrapeSmall(url,amount, italian){
            
            console.log(dataset.url);
           
-         /*  mongo.storeWebpage(dataset,function(err,succ){
+           mongo.storeWebpage(dataset,function(err,succ){
           
             if(err){
                 console.error(err);
             }    
              
-          });*/
+          });
            
        }
     
@@ -59,13 +59,13 @@ function scrapeChefkoch(url,max,italian){
            };
            console.log(dataset.url);
            
-         /* mongo.storeWebpage(dataset,function(err,succ){
+          mongo.storeWebpage(dataset,function(err,succ){
           
             if(err){
                 console.error(err);
             }    
              
-          });*/
+          });
           
           
      });
@@ -121,20 +121,20 @@ function getContent(html){
 // Call functions
 
 // Italian 500
-scrapeAll("http://www.chefkoch.de/rs/s0t29,28/Europa-Italien-Rezepte.html",500,true);
+//scrapeAll("http://www.chefkoch.de/rs/s0t29,28/Europa-Italien-Rezepte.html",500,true);
 
 // International
 // Afrika 100
-//scrapeAll("http://www.chefkoch.de/rs/s0t101/Afrika-Rezepte.html",100,false);
+scrapeAll("http://www.chefkoch.de/rs/s0t101/Afrika-Rezepte.html",100,false);
 
 // Spain 100 
-//scrapeAll("http://www.chefkoch.de/rs/s0t29,43/Europa-Spanien-Rezepte.html",100,false);
+scrapeAll("http://www.chefkoch.de/rs/s0t29,43/Europa-Spanien-Rezepte.html",100,false);
 
 // German 100
-//scrapeAll("http://www.chefkoch.de/rs/s0t29,65/Europa-Deutschland-Rezepte.html",100,false);
+scrapeAll("http://www.chefkoch.de/rs/s0t29,65/Europa-Deutschland-Rezepte.html",100,false);
 
 // French 100
-//scrapeAll("http://www.chefkoch.de/rs/s0t29,84/Europa-Frankreich-Rezepte.html",100,false);
+scrapeAll("http://www.chefkoch.de/rs/s0t29,84/Europa-Frankreich-Rezepte.html",100,false);
 
 // Australia 100
-//scrapeAll("http://www.chefkoch.de/rs/s0t145/Australien-Rezepte.html",100,false);
+scrapeAll("http://www.chefkoch.de/rs/s0t145/Australien-Rezepte.html",100,false);
